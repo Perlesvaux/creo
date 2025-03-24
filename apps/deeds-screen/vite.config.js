2f4f6bdd131b1b6ed7f01fe6fe747aab7200de3a
Percival <95fjpl95@gmail.com>
2025-03-24 13:50:58 -0600

    first commit

---
+++ apps/deeds-screen/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+//import { fileURLToPath } from 'url';
+//import path from 'path';
+
+// Convert the module URL to a file path
+//const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: { port: 5180 }, 
+  preview: { port: 5181 },
+  //publicDir:'../static',
+  //resolve: {
+  //  alias: {
+  //    '@static': path.resolve(__dirname, '../static'),
+  //  },
+  //},
+})
+
+
+    //"build": "vite build && ./integrate.sh",
+
+// import.meta.url: This provides the URL of the current module (in this case, vite.config.js).
+// fileURLToPath: Converts the file:// URL to a file system path.
+// path.dirname: Extracts the directory name from the file path.
---
+++ docs/deeds.markdown
@@ -0,0 +1,10 @@
+---
+layout: simple
+title: Mateo 7:16
+permalink: /deeds/
+---
+
+<script type="module" crossorigin src="{{ site.baseurl }}{% link assets/deeds-screen/index.js %}"></script>
+<link rel="stylesheet" crossorigin href="{{ site.baseurl }}{% link assets/deeds-screen/index.css %}">
+<div id="root-deeds-screen"></div>
+
