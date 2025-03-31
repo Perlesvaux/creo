63e6be70c37cd02145a1cb81e00786d795f07cd2
Percival <95fjpl95@gmail.com>
2025-03-31 04:23:31 -0600

    feat: to_webp compresses multimedia assets

--- apps/hero-screen/src/Showreels.jsx
+++ apps/hero-screen/src/Showreels.jsx
@@ -1,26 +1,26 @@
 import { Card, Picture, Movie } from 'shared'
 // /creo-digital-agency/
-import showreelFront2025    from '/static/showreel_2025/front.png'; 
-import showreelBack2025     from '/static/showreel_2025/back.png';
+import showreelFront2025    from '/static/showreel_2025/front.webp'; 
+import showreelBack2025     from '/static/showreel_2025/back.webp';
 import showreelVideo2025    from '/static/showreel_2025/video.mp4';
-import showreelFront2024    from '/static/showreel_2024/front.png'; 
-import showreelBack2024     from '/static/showreel_2024/back.png';
+import showreelFront2024    from '/static/showreel_2024/front.webp'; 
+import showreelBack2024     from '/static/showreel_2024/back.webp';
 import showreelVideo2024    from '/static/showreel_2024/video.mp4';
-import showreelFront2023    from '/static/showreel_2023/front.png'; 
-import showreelBack2023     from '/static/showreel_2023/back.png';
+import showreelFront2023    from '/static/showreel_2023/front.webp'; 
+import showreelBack2023     from '/static/showreel_2023/back.webp';
 import showreelVideo2023    from '/static/showreel_2023/video.mp4';
-import showreelFront2022    from '/static/showreel_2022/front.png'; 
+import showreelFront2022    from '/static/showreel_2022/front.webp'; 
 import showreelVideo2022    from '/static/showreel_2022/video.mp4'; 
-import showreelFront2021    from '/static/showreel_2021/front.png'; 
-import showreelBack2021     from '/static/showreel_2021/back.png';
+import showreelFront2021    from '/static/showreel_2021/front.webp'; 
+import showreelBack2021     from '/static/showreel_2021/back.webp';
 import showreelVideo2021    from '/static/showreel_2021/video.mp4';
-import showreelFront2020    from '/static/showreel_2020/front.png'; 
-import showreelBack2020     from '/static/showreel_2020/back.png';
+import showreelFront2020    from '/static/showreel_2020/front.webp'; 
+import showreelBack2020     from '/static/showreel_2020/back.webp';
 import showreelVideo2020    from '/static/showreel_2020/video.mp4';
-import showreelFront2016    from '/static/showreel_2016/front.png'; 
-import showreelBack2016     from '/static/showreel_2016/back.png';
+import showreelFront2016    from '/static/showreel_2016/front.webp'; 
+import showreelBack2016     from '/static/showreel_2016/back.webp';
 import showreelVideo2016    from '/static/showreel_2016/video.mp4';
-import posterCREO           from '/static/poster.png'
+import posterCREO           from '/static/poster.webp'
 
 
 const showreel_2025 = () => {
