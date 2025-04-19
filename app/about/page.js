1c40658344d51496cd05f47c686ba5e18fe1ce57
Percival <95fjpl95@gmail.com>
2025-04-19 15:38:50 -0600

    fix: improving css of about/

--- app/about/page.js
+++ app/about/page.js
@@ -14,20 +14,13 @@ export default function About() {
       <h2>Encuentranos</h2>
 
 
-      <div className="flex-1 relative">
-          <a href="https://www.bing.com/maps?where=San%20Salvador%2C%20San%20Salvador%201101%2C%20SV" target="_blank" rel="noopener noreferrer" tabIndex="-1">
-              <div className={css.map}>
-                  <div id="ember1246" className="ember-view   full-height">
-                      <img src="https://dev.virtualearth.net/REST/v1/Imagery/Map/CanvasLight/San%20Salvador,%20San%20Salvador,%201101,%20SV?key=Atn679uHwJz5qBW7Qdydz8-WNiejypvCQM1WRC94l9MtNSsBeNwHcLXzUAfFPaBr&amp;mapSize=760,350" alt="Map of  San Salvador, San Salvador 1101, SV" className="org-location-map__image" />
-                  </div>
-              </div>
-          </a>
-      </div>
-
-
-    <hr/>
 
   </div>
+      <div className={css.map}>
+          <a href="https://www.bing.com/maps?where=San%20Salvador%2C%20San%20Salvador%201101%2C%20SV" target="_blank" rel="noopener noreferrer" tabIndex="-1">
+                      <img src="https://dev.virtualearth.net/REST/v1/Imagery/Map/CanvasLight/San%20Salvador,%20San%20Salvador,%201101,%20SV?key=Atn679uHwJz5qBW7Qdydz8-WNiejypvCQM1WRC94l9MtNSsBeNwHcLXzUAfFPaBr&amp" alt="Map of  San Salvador, San Salvador 1101, SV" className="org-location-map__image" />
+          </a>
+      </div>
 
   </section>
 }
