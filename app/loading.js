505f5ff0c04017bf64d626cfb11ac0c12aabcad5
Percival <95fjpl95@gmail.com>
2025-04-27 19:20:23 -0600

    fix: css of loading component improved

--- app/loading.js
+++ app/loading.js
@@ -1,9 +1,10 @@
 // app/loading.js
+import Image from 'next/image'
+import css from './loading.module.css'
 export default function Loading() {
   return (
-    <div className="">
-      LOADING ... 
-
+    <div className={css.loading}>
+      <Image src="/assets/favicon.svg" height={120} width={120} alt="CREO" className={css.icon} />
     </div>
   )
 }
